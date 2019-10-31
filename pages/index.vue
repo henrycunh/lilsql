c33b81127442fed0cda0f4e1cfe777c76f4532ec
Iago Elias <iago@sflabs.com.br>
2019-10-31 20:18:28 -0300

    Documentation improved

---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <TopBar/>
+    <section class="top-section">
+      <Description
+        title='O que fazemos.'
+        description='A BT Científica é uma startup focada em pesquisa e desenvolvimento de processos e/ou produtos químicos e farmacêuticos baseados em Nanotecnologia.'
+      />
+      <CardHolder/>
+    </section>
+    <PersonCardHolder/>
+    <footer class = "info">
+      <Footer 
+        email="btpesquisa.cientifica@gmail.com"	
+        tel="(16) 991315332"
+      />
+    </footer>
+  </div>
+</template>
+
+<script>
+import Footer from '~/components/Footer.vue'
+import CardHolder from '~/components/CardHolder.vue'
+import TopBar from '~/components/TopBar.vue'
+import Description from '~/components/Description.vue'
+import PersonCardHolder from '~/components/PersonCardHolder.vue'
+
+export default {
+  components: {
+    CardHolder,
+    Footer,
+    TopBar,
+    Description,
+    PersonCardHolder
+  }
+}
+</script>
+
+<style scoped>
+
+.top-section {
+  background-color: white;
+  padding-bottom: 30px;
+}
+.container {
+  font-family: Roboto, sans-serif;
+  position: absolute;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  right: 0;
+  max-width: 100%;
+  background-color: #0d223b;
+  text-align: center;
+}
+
+.info {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #0d223b;
+  padding-top: 5px;
+  padding-bottom: 10px;
+  width: 100%;
+  min-width: 100%;
+}
+
+</style>
